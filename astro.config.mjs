4fc8a554a3ce154988a979c551f7a4fc2f9728c8
eduinside <42594552+eduinside@users.noreply.github.com>
2026-01-06 00:20:23 +0900

    First Edition of my site (#1)
    * Update index.mdx
    
    * Create notion-styler.md
    
    * Update astro.config.mjs
    
    * Update astro.config.mjs
    
    * Create notion-styler.astro
    
    * Create notion-styler.html
    
    * Create index.html
    
    * Delete public/notion-styler.html
    
    * Update astro.config.mjs
    
    * Update astro.config.mjs
    
    * Update astro.config.mjs
    
    * Update astro.config.mjs
    
    * Delete src/pages directory
    
    * Update index.html
    
    * Update astro.config.mjs
    
    * Update astro.config.mjs
    
    * Changes 0105
    
    * link changes
    
    * change links
    
    * renamed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,15 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'byeduin VIVES',
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Intro',
+					link: '/intro',
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Apps',
+					autogenerate: { directory: 'app' },
 				},
 			],
 		}),
---
+++ src/content/docs/intro.md
@@ -0,0 +1,5 @@
+---
+title: App Intro
+description: Welcome
+---
+Welcome to my VIVES. Select my apps on the sidebar.
